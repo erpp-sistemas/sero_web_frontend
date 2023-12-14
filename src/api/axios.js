b9b279eaa548d7ff85b25020d0832c99af1942bc
DavidDemetrio <davidlopez.paz26@outook.es>
2023-12-14 11:34:16 -0600

    Change in .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    //baseURL: 'https://erpp.center/sero-web/api',
-    //withCredentials: true
-})
+const instance = axios.create({ baseURL: import.meta.env.VITE_BASE_URL})
 
 export default instance
