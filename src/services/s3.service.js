7489b81b291b7dcee3689cbcb35242c0017a97e1
ratel4150 <rratel76@gmail.com>
2023-12-22 14:45:36 -0600

    ## **Título del Commit**
    **Añadir funciones utilitarias para AWS S3: listar objetos y cargar archivos**
    
    ## **Descripción del Commit**
    
    Este commit introduce dos nuevas funciones utilitarias para interactuar con AWS S3:
    
    ### **Función `listObjects`**
    - La función `listObjects` utiliza el comando `ListObjectsV2Command` para obtener una lista de objetos en el bucket de S3 especificado (`AWS_BUCKET_NAME`).
    - Las claves de los objetos se imprimen en la consola.
    
    ### **Función `uploadToS3`**
    - La función `uploadToS3` carga un archivo al bucket de S3 especificado (`AWS_BUCKET_NAME`) utilizando el comando `PutObjectCommand`.
    - Después de una carga exitosa, genera una URL firmada con una vigencia de una semana utilizando `getSignedUrl`.
    - La URL firmada se imprime en la consola.
    - Cualquier error durante el proceso se captura, registra y vuelve a lanzar.
    
    ### **Configuración de AWS SDK**
    - Se importaron los módulos necesarios del paquete `@aws-sdk/client-s3` y `@aws-sdk/s3-request-presigner`:
      ```javascript
      import { S3Client, HeadBucketCommand, ListObjectsV2Command, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
      import { getSignedUrl } from "@aws-sdk/s3-request-presigner";

---
+++ src/services/s3.service.js
@@ -0,0 +1,60 @@
+import { S3Client, HeadBucketCommand,ListObjectsV2Command, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+const AWS_BUCKET_NAME = "bucket-files-msg";
+const AWS_BUCKET_REGION = "us-east-2";
+const AWS_PUBLIC_KEY = "AKIAT5FEWJS6OYP6WLHR";
+const AWS_SECRET_KEY = "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";
+
+const client = new S3Client({
+  region: AWS_BUCKET_REGION,
+  credentials: {
+    accessKeyId: AWS_PUBLIC_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+  },
+});
+
+
+export const listObjects = async () => {
+  try {
+    const command = new ListObjectsV2Command({
+      Bucket: AWS_BUCKET_NAME,
+    });
+
+    const response = await client.send(command);
+
+    // Imprimir información sobre los objetos en la consola
+    console.log("Objetos en el bucket:");
+    response.Contents.forEach((object) => {
+      console.log(object.Key);
+    });
+  } catch (err) {
+    console.error("Error al listar objetos:", err.stack);
+  }
+};
+
+
+
+
+export const uploadToS3 = async (file) => {
+  try {
+    const params = {
+      Bucket: AWS_BUCKET_NAME,
+      Key: file.name,
+      Body: file,
+    };
+
+    const command = new PutObjectCommand(params);
+    await client.send(command);
+
+    // Firmar la URL con una vigencia de una semana (604800 segundos)
+    const signer = await getSignedUrl(client, new GetObjectCommand({ Bucket: AWS_BUCKET_NAME, Key: file.name }), { expiresIn: 604800 });
+
+    // Construir y devolver la URL del objeto subido
+    const fileUrl = `${signer}`;
+    console.log("Archivo subido exitosamente. URL firmada:", fileUrl);
+    return fileUrl;
+  } catch (err) {
+    console.error("Error al subir archivo:", err.stack);
+    throw err;
+  }
+};
