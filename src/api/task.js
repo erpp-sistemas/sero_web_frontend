565d81e05f297222bb2d5bb6a256cb6dfb98e2fb
ratel4150 <rratel76@gmail.com>
2023-12-22 16:35:18 -0600

    feat(task): Implementar operaciones CRUD para tareas
    Este commit introduce funciones para realizar las operaciones básicas de un CRUD (Crear, Leer, Actualizar, Eliminar) relacionadas con tareas.
    
    1. **Crear tarea:** Agrega la función `createTask` para crear nuevas tareas con los datos proporcionados.
    
    2. **Leer tarea por ID:** Agrega la función `getTaskById` para obtener detalles de una tarea específica por su ID.
    
    3. **Actualizar tarea por ID:** Agrega la función `updateTaskById` para modificar detalles de una tarea específica por su ID.
    
    4. **Eliminar tarea por ID:** Agrega la función `deleteTaskById` para eliminar una tarea según su ID.
    
    Cada función maneja errores de manera adecuada y permite realizar operaciones adicionales antes o después de ejecutar la operación CRUD correspondiente.

---
+++ src/api/task.js
@@ -0,0 +1,51 @@
+
+import axios from './axios'; // Asegúrate de que el path sea correcto
+
+// Función para obtener todas las tareas de un servicio por ID
+export const getAllTasksByServiceId = async (serviceId) => {
+    try {
+      const response = await axios.get(`/services/${serviceId}`);
+      // Podrías realizar operaciones adicionales aquí antes de retornar los datos
+      return response.data;
+    } catch (error) {
+      console.error("Error al obtener todas las tareas:", error.response?.data || error.message);
+      throw error.response?.data || error;
+    }
+  };
+
+  // Función para actualizar una tarea por su ID
+export const updateTaskById = async (taskId, updatedTaskData) => {
+    try {
+      const response = await axios.put(`/tasks/${taskId}`, updatedTaskData);
+      // Podrías realizar operaciones adicionales aquí después de la actualización
+      return response.data;
+    } catch (error) {
+      console.error("Error al actualizar la tarea por ID:", error.response?.data || error.message);
+      throw error.response?.data || error;
+    }
+  };
+
+
+// Función para eliminar una tarea por su ID
+export const deleteTaskById = async (taskId) => {
+    try {
+      const response = await axios.delete(`/tasks/${taskId}`);
+      // Podrías realizar operaciones adicionales aquí después de la eliminación
+      return response.data;
+    } catch (error) {
+      console.error("Error al eliminar la tarea por ID:", error.response?.data || error.message);
+      throw error.response?.data || error;
+    }
+  };
+  
+  // Función para crear una nueva tarea
+  export const createTask = async (taskData) => {
+    try {
+      const response = await axios.post('/task', taskData);
+      // Podrías realizar operaciones adicionales aquí después de la creación
+      return response.data;
+    } catch (error) {
+      console.error("Error al crear una nueva tarea:", error.response?.data || error.message);
+      throw error.response?.data || error;
+    }
+  };
