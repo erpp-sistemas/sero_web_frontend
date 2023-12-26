0f12972b6549a5259dddf4b69883c5939a950a89
ratel4150 <rratel76@gmail.com>
2023-12-26 12:56:51 -0600

    feat: Agregar componente ServiceCrud a la página de gestión de servicios
    Se incorpora el componente ServiceCrud en la página principal de gestión de servicios. El componente proporciona la estructura básica para realizar operaciones CRUD relacionadas con servicios en la interfaz de usuario. Además, se añade un log de prueba en la consola para verificar el correcto funcionamiento.

--- src/scenes/service/index.jsx
+++ src/scenes/service/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { tokens } from '../../theme';
 import Header from '../../components/Header';
+import ServiceCrud from '../../components/ServiceCrud';
 /**
  * Página principal para la gestión de servicios.
  *
@@ -17,7 +18,7 @@ console.log("prueba");
   return (
     <Box m="20px">
     <Header title="Gestiòn de Servicios" subtitle="Operaciones de Crear, Leer, Actualizar y Eliminar Servicios en el Sistema" />
- ServiceCrud
+ <ServiceCrud/>
     
   </Box>
   )
