26714a76a7309586dbd3298748095f75ac9f7a57
ratel4150 <rratel76@gmail.com>
2023-12-26 12:53:21 -0600

    feat: Agregar componente ServiceCrud
    Añade el componente ServiceCrud, diseñado para administrar servicios en la interfaz de usuario. Este componente aún no implementa funcionalidades específicas, pero proporciona la estructura básica para futuras operaciones CRUD relacionadas con servicios.

---
+++ src/components/ServiceCrud/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+/**
+ * componente para administrar servicios .
+ *
+ * @component
+ * @return {JSX.Element} componente ServiceCrud
+ */
+
+function ServiceCrud() {
+  return (
+    <Container>
+        ServiceCrud
+    </Container>
+  )
+}
+
+export default ServiceCrud
