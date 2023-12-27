0273bc9b13ed475990da462aece4a5b8b4d4e409
ratel4150 <rratel76@gmail.com>
2023-12-27 14:04:28 -0600

    Agregar componente ProcessCrud
    Este commit introduce el componente ProcessCrud, que representa la sección de CRUD (Crear, Leer, Actualizar, Eliminar) de la gestión de procesos. El componente utiliza un contenedor (Container) para estructurar su contenido.
    
    ProcessCrud proporciona la base para las operaciones CRUD en la gestión de procesos. Futuras actualizaciones y funcionalidades relacionadas con la gestión de procesos se implementarán en esta sección del código.
    
    Nota: Asegúrate de reemplazar 'Ruta/Al/Archivo/ProcessCrud.js' con la ruta real de tu archivo.

---
+++ src/components/ProcessCrud/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from '../Container'
+
+/**
+ * Componente que representa la sección de CRUD (Crear, Leer, Actualizar, Eliminar) de la gestión de procesos.
+ *
+ * @component
+ * @returns {JSX.Element} Componente ProcessCrud.
+ */
+function ProcessCrud() {
+  return (
+    <Container>
+      ProcessCrud
+    </Container>
+  )
+}
+
+export default ProcessCrud
--- src/scenes/process/index.jsx
+++ src/scenes/process/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from '../../components/Header'
 import { useSelector } from 'react-redux';
 import { tokens } from '../../theme';
+import ProcessCrud from '../../components/ProcessCrud';
 /**
  * Página principal para la gestión de procesos.
  *
@@ -16,7 +17,7 @@ function Process() {
   return (
     <Box m="20px">
     <Header title="Gestiòn de Procesos" subtitle="Operaciones de Crear, Leer, Actualizar y Eliminar Procesos en el Sistema" />
-  ProcessCrud
+  <ProcessCrud/>
     
   </Box>
   )
