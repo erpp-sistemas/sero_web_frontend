cd7a2f5aebbc5cac174ac6c0f325703e0a732905
ratel4150 <rratel76@gmail.com>
2023-12-27 13:55:36 -0600

    Añadir página de gestión de procesos
    Esta actualización agrega la página de gestión de procesos que incluye un encabezado, utilizando el componente Header, y un esbozo del contenido principal. Se utilizan los hooks useSelector y useTheme para acceder al estado del usuario y al tema de Material-UI, respectivamente.
    
    El componente Process se utilizará para operaciones de Crear, Leer, Actualizar y Eliminar (CRUD) en el sistema. La información sobre el usuario y los colores del tema se obtiene mediante el uso de Redux y tokens del tema.
    
    Cualquier cambio adicional relacionado con la gestión de procesos se integrará en esta página.

---
+++ src/scenes/process/index.jsx
@@ -0,0 +1,25 @@
+import { Box, useTheme } from '@mui/material'
+import React from 'react'
+import Header from '../../components/Header'
+import { useSelector } from 'react-redux';
+import { tokens } from '../../theme';
+/**
+ * Página principal para la gestión de procesos.
+ *
+ * @component
+ * @returns {JSX.Element} Componente Process.
+ */
+function Process() {
+    const user = useSelector((state) => state.user);
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+    <Header title="Gestiòn de Procesos" subtitle="Operaciones de Crear, Leer, Actualizar y Eliminar Procesos en el Sistema" />
+  ProcessCrud
+    
+  </Box>
+  )
+}
+
+export default Process
