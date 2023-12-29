1094041165a6c76092d9d4c22a0d6d945f2f088f
ratel4150 <rratel76@gmail.com>
2023-12-29 17:46:32 -0600

    Implementar funcionalidad de lectura de menús con Material-UI DataGrid
    Este commit introduce una nueva característica para leer y mostrar menús utilizando el DataGrid de Material-UI. Los cambios principales incluyen:
    
    - Se agregó un nuevo componente
    - Se utilizó la función `getAllMenus` para recuperar todos los menús desde el servidor.
    - Se integró el DataGrid para presentar la información del menú con columnas como ID de menú, nombre, descripción y cualquier otro detalle relevante.
    - Se actualizaron los estilos y el diseño para garantizar una presentación limpia y amigable de la información del menú.
    
    Esta mejora proporciona una vista integral de los menús para un usuario específico, mejorando la interfaz de usuario y la experiencia.

--- src/components/MenuCrud/components/DataGridMenuCrud/index.jsx
+++ src/components/MenuCrud/components/DataGridMenuCrud/index.jsx
@@ -1,8 +1,207 @@
 import React from 'react'
-
+import { getAllMenus } from '../../../../api/menu';
+import { Box, IconButton } from '@mui/material';
+import CheckIcon from "@mui/icons-material/Check";
+import ClearIcon from "@mui/icons-material/Clear";
+import { DataGrid } from '@mui/x-data-grid';
+  /**
+   * CheckCell component for rendering an IconButton with check or clear icon based on data.
+   *
+   * @component
+   * @param {Object} props - The component props.
+   * @param {boolean} props.data - Boolean data to determine the icon.
+   * @returns {JSX.Element} - The rendered CheckCell component.
+   *
+   * @example
+   * // Usage example
+   * <CheckCell data={true} />
+   */
+  const CheckCell = ({ data }) => {
+    if (data) {
+      return (
+        <IconButton aria-label="check" size="small">
+          <CheckIcon fontSize="inherit" color="secondary" />
+        </IconButton>
+      );
+    } else {
+      return (
+        <IconButton aria-label="check" size="small">
+          <ClearIcon fontSize="inherit" sx={{ color: "red" }} />
+        </IconButton>
+      );
+    }
+  };
 function DataGridMenuCrud() {
+  const [rows, setRows] = React.useState([]);
+
+
+  const fetchMenus = async () => {
+    try {
+      // Aquí deberías hacer tu solicitud de red para obtener los datos
+      // Reemplaza 'TU_URL_DE_DATOS' con la URL real de tus datos
+      const response = await getAllMenus();
+
+      // Agrega el campo 'id_menu' a cada fila usando el índice como valor único si no no se ven en la datagrid
+      const rowsWithId = response.map((row, index) => ({
+        ...row,
+        id: row.id_menu ,
+      }));
+
+      setRows(rowsWithId);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }; 
+
+  
+  React.useEffect(() => {
+    /**
+     * Función asíncrona para obtener y establecer los datos de las tareas.
+     *
+     * @function
+     * @async
+     * @private
+     */
+
+    fetchMenus();
+  }, []);
+
+  const buildColumns = () => {
+    const columns = [ {
+      field: "nombre",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Nombre"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 180,
+      editable: true,
+    }, 
+    {
+      field: "id_menu_padre",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Menu Padre"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 180,
+      editable: true,
+    },
+    {
+      field: "descripcion",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Descripción"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 180,
+      editable: true,
+    },
+  
+    {
+      field: "url",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Url"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 180,
+      editable: true,
+    },
+    {
+      field: "icono",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Icono"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 180,
+      editable: true,
+    },
+    {
+      field: "url",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Url"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 180,
+      editable: true,
+    },
+    {
+      field: "activo",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Estado"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 80,
+      type: "boolean",
+      editable: true,
+      renderCell: (params) => <CheckCell data={params.row.activo} />,
+    },{
+      field: "icon_mui",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Icon Mui"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 180,
+      editable: true,
+    },
+    {
+      field: "route",
+      renderHeader: () => (
+        <strong style={{ color: "#5EBFFF" }}>
+          {"Ruta"}
+          {/*    <span role="img" aria-label="task" style={{color:"#5EBFFF"}}>
+        📃
+        </span> */}
+        </strong>
+      ),
+      width: 180,
+      editable: true,
+    }]
+    return columns
+  }
+
+
   return (
-    <div>DataGridMenuCrud</div>
+    <Box sx={{ height: 400, width: "100%",'.css-196n7va-MuiSvgIcon-root': {
+      fill:"white"
+    }  }}>
+    
+      <DataGrid
+        
+        rows={rows}
+        columns={buildColumns()}
+       
+      />
+      </Box>
   )
 }
 
