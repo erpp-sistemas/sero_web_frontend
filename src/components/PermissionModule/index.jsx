2abb0b19262d19f67ea004956dcfe4afdd6a11f1
ratel4150 <rratel76@gmail.com>
2024-01-17 17:56:03 -0600

    Se crea la funcion para crear relacion rolo-submenu asi como tambien se arregla el bug de los submenus

--- src/components/PermissionModule/index.jsx
+++ src/components/PermissionModule/index.jsx
@@ -28,6 +28,7 @@ import { getAllSubMenus } from "../../api/submenu";
 import { getAllRoles } from "../../api/rol";
 import {
   createMenuRol,
+  createSubMenuRol,
   getMenuRolByIdRol,
   getSubMenuRolByIdRol,
   updateMenuRolById,
@@ -38,6 +39,10 @@ import { GrServices } from "react-icons/gr";
 import { FaRegCircleCheck } from "react-icons/fa6";
 import { AddOutlined, Sync, SyncAltOutlined } from "@mui/icons-material";
 import { PiTreeStructureFill } from "react-icons/pi";
+
+
+
+
 
 /**
  * Modulo
@@ -56,8 +61,16 @@ function PermissionModule() {
   const [snackbar, setSnackbar] = React.useState(null);
   const [isNewMenuRolDialogOpen, setIsNewMenuRolDialogOpen] =
     React.useState(false);
+    const [isNewSubMenuRolDialogOpen, setIsNewSubMenuRolDialogOpen] =
+    React.useState(false);
   const [menuRolData, setMenuRolData] = React.useState({
     id_menu: "",
+    id_rol: "",
+    activo: "",
+  });
+
+  const [subMenuRolData, setSubMenuRolData] = React.useState({
+    id_sub_menu: "",
     id_rol: "",
     activo: "",
   });
@@ -83,6 +96,49 @@ function PermissionModule() {
       [name]: newValue,
     }));
   };
+
+
+
+   /**
+   * Manejador de eventos que se ejecuta cuando se produce un cambio en los campos de entrada del formulario.
+   *
+   * @param {Object} event - Objeto del evento que contiene información sobre el cambio.
+   * @param {string} event.target.name - Nombre del campo que ha cambiado.
+   * @param {string} event.target.value - Valor actual del campo que ha cambiado.
+   * @param {string} event.target.type - Tipo del campo que ha cambiado.
+   * @param {boolean} event.target.checked - Estado de verificación en caso de un campo de tipo checkbox.
+   *
+   * @function
+   */
+   const handleInputSubMenuOnChange = (event) => {
+    const { name, value, type, checked } = event.target;
+
+    // Actualiza el estado serviceData con el nuevo valor del campo Servicio
+    const newValue = type === "checkbox" ? checked : value;
+    setSubMenuRolData((prevState) => ({
+      ...prevState,
+      [name]: newValue,
+    }));
+  };
+
+
+  /**
+ * Manejador de eventos que se ejecuta al abrir el diálogo para la creación de un nuevo SubMenuRol.
+ * @function
+ * @returns {void}
+ */
+const handleOpenNewSubMenuRolDialog = () => {
+  setIsNewSubMenuRolDialogOpen(true);
+};
+
+/**
+ * Manejador de eventos que se ejecuta al cerrar el diálogo para la creación de un nuevo SubMenuRol.
+ * @function
+ * @returns {void}
+ */
+const handleCloseNewSubMenuRolDialog = () => {
+  setIsNewSubMenuRolDialogOpen(false);
+};
 
   /**
    * Maneja la apertura del diálogo para la creación de un nuevo menú_rol.
@@ -303,15 +359,22 @@ function PermissionModule() {
     fetchSubMenus();
     fetchRoles();
   }, [selectedRole]);
-  const filteredSubMenusByRole = (menuId) => {
+  const filteredSubMenusByRole = (menuId, idRol) => {
     // Filtrar submenús basados en el menú padre (menuId)
     const subMenusForMenu = subMenus.filter((subMenu) => subMenu.id_menu_padre === menuId);
   
-    // Filtrar los submenús que están asociados al rol actual
-    return subMenusForMenu.filter((subMenu) => {
-      const subMenuRol = subMenuRols.find((subMenuRol) => subMenuRol.id_sub_menu === subMenu.id);
-      return subMenuRol && subMenuRol.activo;
+    // Mapear los submenús y agregar la propiedad 'activo' a cada uno
+    const subMenusWithActiveFlag = subMenusForMenu.map((subMenu) => {
+      const subMenuRol = subMenuRols.find((subMenuRol) => subMenuRol.id_sub_menu === subMenu.id && subMenuRol.id_rol === idRol);
+      const isActive = subMenuRol ? subMenuRol.activo : false;
+  
+      return {
+        ...subMenu,
+        activo: isActive,
+      };
     });
+  
+    return subMenusWithActiveFlag;
   };
 
  /*  const children = (
@@ -358,6 +421,45 @@ function PermissionModule() {
       // Cerrar el diálogo, actualizar el estado, o realizar otras acciones necesarias
       fetchMenuByRolId();
       handleCloseNewMenuRolDialog();
+    } catch (error) {
+      console.error("Error al guardar datos:", error);
+      setSnackbar({ children: "Error al guardar datos", severity: "error" });
+      // Aquí puedes manejar el error según tus necesidades
+    }
+  };
+
+
+
+
+  /**
+   * Manejador de eventos que se ejecuta al intentar agregar un nuevo rol.
+   * Verifica la validez de los campos del formulario y realiza la acción correspondiente.
+   *
+   * @async
+   * @function
+   */
+  const handleAddSubMenuRol = async () => {
+    /**
+     * Verifica si todos los campos del formulario están validados.
+     *
+     * @type {boolean} Indica si todos los campos están validados.
+     */
+
+    try {
+      const response = await createSubMenuRol(subMenuRolData);
+
+      // Aquí puedes manejar la respuesta de la solicitud si es necesario
+      console.log("Respuesta de la API:", response.data);
+
+      // Mostrar Snackbar de éxito
+      setSnackbar({
+        children: "Asociaciòn entre sub-menu y rol exitoso",
+        severity: "success",
+      });
+
+      // Cerrar el diálogo, actualizar el estado, o realizar otras acciones necesarias
+      fetchSubMenuByRolId();
+      handleCloseNewSubMenuRolDialog();
     } catch (error) {
       console.error("Error al guardar datos:", error);
       setSnackbar({ children: "Error al guardar datos", severity: "error" });
@@ -468,39 +570,53 @@ function PermissionModule() {
           >
             Ligar Rol-Menu
           </Button>
+          <Button
+          onClick={handleOpenNewSubMenuRolDialog}
+            variant="outlined"
+            size="small"
+            color="secondary"
+            startIcon={<AddOutlined />}
+            endIcon={<PiTreeStructureFill />}
+          >
+            Ligar Rol-SubMenu
+          </Button>
           {menus.map((menu) => {
-            const menuRol = menuRols.find((mr) => mr.id_menu === menu.id);
-            const isChecked = menuRol ? menuRol.activo : false;
+  const menuRol = menuRols.find((mr) => mr.id_menu === menu.id);
+  const isChecked = menuRol ? menuRol.activo : false;
 
-            return (
-              <FormGroup key={menu.id}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      color="secondary"
-                      checked={isChecked}
-                      onChange={(event) => {
-                        // Handle checkbox change here
-                        handleCheckboxChange(menu.id, event.target.checked);
-                      }}
-                    />
-                  }
-                  label={menu.nombre}
-                />
-                {filteredSubMenusByRole(menu.id).map((subMenuRol) => (
+  return (
+    <FormGroup key={menu.id}>
+      <FormControlLabel
+        control={
+          <Checkbox
+            color="secondary"
+            checked={isChecked}
+            onChange={(event) => {
+              handleCheckboxChange(menu.id, event.target.checked);
+            }}
+          />
+        }
+        label={menu.nombre}
+      />
+      {filteredSubMenusByRole(menu.id, selectedRole).map((subMenu) => (
         <FormControlLabel
-          key={subMenuRol.id}
+          key={subMenu.id}
           sx={{ marginLeft: "2rem" }}
-          control={<Checkbox defaultChecked={subMenuRol.activo}   onChange={(event) => {
-            // Handle checkbox change here
-            handleSubMenuCheckboxChange(subMenuRol.id, event.target.checked);
-          }} color="secondary" />}
-          label={`${subMenuRol.nombre}`}
+          control={
+            <Checkbox
+              checked={subMenu.activo}
+              onChange={(event) => {
+                handleSubMenuCheckboxChange(subMenu.id, event.target.checked);
+              }}
+              color="secondary"
+            />
+          }
+          label={`${subMenu.nombre}`}
         />
       ))}
-              </FormGroup>
-            );
-          })}
+    </FormGroup>
+  );
+})}
        
         
         </Box>
@@ -658,6 +774,158 @@ function PermissionModule() {
                   onClick={handleAddMenuRol}
                 >
                   Guardar Asociaciòn de Rol con Menu
+                </Button>
+              </Box>
+            </Paper>
+          </Box>
+        </Dialog>
+      )}
+      {isNewSubMenuRolDialogOpen && (
+        <Dialog
+          fullScreen
+          open={isNewSubMenuRolDialogOpen}
+          onClose={handleCloseNewSubMenuRolDialog}
+        >
+          <AppBar sx={{ position: "relative" }}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={handleCloseNewSubMenuRolDialog}
+                aria-label="close"
+              >
+                <CloseIcon />
+              </IconButton>
+              {/*  <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                Agrega nueva tarea
+              </Typography> */}
+              {/*  <Button autoFocus color="inherit"  onClick={handleClose}>
+                Guardar
+              </Button> */}
+            </Toolbar>
+          </AppBar>
+          {/* Aqui va el contenido */}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%", // Ajusta según sea necesario
+            }}
+          >
+            <Paper
+              sx={{
+                width: "40%",
+                height: "auto",
+                boxShadow: 3,
+                padding: "2rem",
+                borderRadius: 1,
+              }}
+            >
+              {/* Contenido real del Paper */}
+              <Typography variant="body1" sx={{ mb: "2rem" }}>
+                Agregar nueva asociaciòn sub-menu rol
+              </Typography>
+              {/* nombre, :imagen, :activo, :orden, :icono_app_movil */}
+              <Grid container spacing={2}>
+                <Grid item xs={12} sx={{ p: 2 }}>
+                  <FormControl variant="filled" sx={{ m: 1, minWidth: 300 }}>
+                    <InputLabel id="demo-simple-select-filled-label">
+                      Rol
+                    </InputLabel>
+                    <Select
+                      sx={{ width: 450 }}
+                      color="secondary"
+                      labelId="demo-simple-select-filled-label-id-rol"
+                      id="demo-simple-select-filled-id-rol"
+                      name="id_rol"
+                       value={subMenuRolData.id_rol}
+                      onChange={handleInputSubMenuOnChange} 
+                    >
+                      <MenuItem value="">
+                        <em>Ningun</em>
+                      </MenuItem>
+                      {roles.map((role) => (
+                        <MenuItem key={role.id} value={role.id}>
+                          {role.nombre}{" "}
+                          {/* Replace with the actual property name from your role object */}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+
+                  <FormControl variant="filled" sx={{ m: 1, minWidth: 300 }}>
+                    <InputLabel id="demo-simple-select-filled-label">
+                      SubMenu
+                    </InputLabel>
+                    <Select
+                      sx={{ width: 450 }}
+                      color="secondary"
+                      labelId="demo-simple-select-filled-label-id-menu"
+                      id="demo-simple-select-filled-id-menu"
+                      name="id_sub_menu"
+                    value={subMenuRolData.id_sub_menu}
+                      onChange={handleInputSubMenuOnChange} 
+                    >
+                      <MenuItem value="">
+                        <em>Ningun</em>
+                      </MenuItem>
+                      {subMenus.map((subMenu) => (
+                        <MenuItem key={subMenu.id} value={subMenu.id}>
+                          {subMenu.nombre}{" "}
+                          {/* Replace with the actual property name from your role object */}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  {/*   {validateInputs.nombre ? (
+                    <Stack sx={{ marginTop: "0.2rem" }} direction="row">
+                      <FaRegCircleCheck style={{ color: "#14B814" }} />{" "}
+                      <Typography color={"secondary"} variant="caption">
+                        ¡Gracias por ingresar un rol!
+                      </Typography>
+                    </Stack>
+                  ) : (
+                    <Typography sx={{ color: "red" }} variant="caption">
+                      * ¡Por favor, ingresa un rol!
+                    </Typography>
+                  )}  */}
+
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      alignContent: "center",
+                      marginBottom: "2rem",
+                      p: 2,
+                    }}
+                  >
+                    <InputLabel sx={{ alignSelf: "center" }}>Activo</InputLabel>
+                    <Checkbox
+                      {..."label"}
+                      onChange={handleInputSubMenuOnChange}
+                      name="activo"
+                      size="small"
+                      color="secondary"
+                    />
+                  </Box>
+                </Grid>
+              </Grid>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "end",
+                  marginTop: "2.5rem",
+                }}
+              >
+                <Button
+                  endIcon={<Sync />}
+                  color="secondary"
+                  variant="contained"
+                 onClick={handleAddSubMenuRol} 
+                >
+                  Guardar Asociaciòn de Rol con Sub-Menu
                 </Button>
               </Box>
             </Paper>
