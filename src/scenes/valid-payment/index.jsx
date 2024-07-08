3faf97487cb495817af6a9f51a135db51116e211
Carlos Adrian Martinez Flores <carlos.martinez@erpp.mx>
2024-07-08 13:27:28 -0600

    .

--- src/scenes/valid-payment/index.jsx
+++ src/scenes/valid-payment/index.jsx
@@ -268,23 +268,23 @@ const Index = () => {
             const headers = Object.keys(result[0]);
             worksheet.addRow(headers);
         
-          result.forEach(row => {
-            const values = headers.map(header => {
-                let value = row[header];
-                if (header === "fecha de pago" || header === "fecha_de_gestion") { 
-                    const date = new Date(value);
-                    const year = date.getUTCFullYear();
-                    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-                    const day = String(date.getUTCDate()).padStart(2, '0');
-                    const hours = String(date.getUTCHours()).padStart(2, '0');
-                    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-                    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-                    value = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-                }
-                return value;
-            });
-            worksheet.addRow(values);
-        });
+        //   result.forEach(row => {
+        //     const values = headers.map(header => {
+        //         let value = row[header];
+        //         if (header === "fecha de pago" || header === "fecha_de_gestion") { 
+        //             const date = new Date(value);
+        //             const year = date.getUTCFullYear();
+        //             const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+        //             const day = String(date.getUTCDate()).padStart(2, '0');
+        //             const hours = String(date.getUTCHours()).padStart(2, '0');
+        //             const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+        //             const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+        //             value = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        //         }
+        //         return value;
+        //     });
+        //     worksheet.addRow(values);
+        // });
                 
             if(filter === 1)
             {
