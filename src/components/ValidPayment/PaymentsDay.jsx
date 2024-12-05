098f657ef30a1b4e8fad1628a066e755bdaaa11b
Carlos Adrian Martinez Flores <carlos.martinez@erpp.mx>
2024-12-05 14:06:28 -0600

    ajuste parametros dinamicos grafica lightweight

--- src/components/ValidPayment/PaymentsDay.jsx
+++ src/components/ValidPayment/PaymentsDay.jsx
@@ -3,26 +3,28 @@ import React from "react";
 import Legend from "../../components/LightweightCharts/Legend";
 
 function PaymentsDay({ data }) {
+  const title = "Pagos por dia";
+  const title_label = "Total pagado";
+  const format = "currency"
   return (
     <Box
       id="grid-1"
       display="grid"
-      gridTemplateColumns="repeat(12, 1fr)"      
+      gridTemplateColumns="repeat(12, 1fr)"
       gap="15px"
     >
       <Box
         gridColumn="span 12"
         backgroundColor="rgba(128, 128, 128, 0.1)"
         borderRadius="8px"
-        sx={{ 
+        sx={{
           cursor: "pointer",
-          borderRadius: '8px', 
-          boxShadow: 3, 
-          padding: 0
+          borderRadius: "8px",
+          boxShadow: 3,
+          padding: 0,
         }}
-        
       >
-        <Legend data={ data } />
+        <Legend data={data} title={title} title_label={title_label} format={ format } />
       </Box>
     </Box>
   );
