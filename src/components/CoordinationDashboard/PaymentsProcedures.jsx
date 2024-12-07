e0dcfaf156167816af7ba0484168b94b7617f1dc
Carlos Adrian Martinez Flores <carlos.martinez@erpp.mx>
2024-12-06 22:59:15 -0600

    ajuste estilo medidor bateria

--- src/components/CoordinationDashboard/PaymentsProcedures.jsx
+++ src/components/CoordinationDashboard/PaymentsProcedures.jsx
@@ -3,6 +3,7 @@ import { Box, useTheme, Button, Avatar, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import { tokens } from "../../theme";
 import Bar from "../NivoChart/Bar";
+import ResponsiveBarChart from '../../components/Charts/NivoCharts/ResponsiveBarChart.jsx'
 
 function PaymentsProcedures({ data }) {
   const theme = useTheme();
@@ -11,6 +12,20 @@ function PaymentsProcedures({ data }) {
   if (!data) {
     return null;
   }
+
+  const formatData = (data) => {
+    return data.map((item) => ({
+      concept: item.concept, // Mantener el índice original
+      gestiones_con_pago: item.gestiones_con_pago,
+      gestiones_con_pagoColor: colors.accentGreen[100], // Asignar color personalizado
+      gestiones_sin_pago: item.gestiones_sin_pago,
+      gestiones_sin_pagoColor: colors.blueAccent[500], // Asignar color personalizado
+    }));
+  };
+
+  const formattedData = formatData(data);
+
+  console.log(formattedData);
 
   const conceptNamesKeys = ["gestiones_con_pago", "gestiones_sin_pago"];
 
@@ -44,18 +59,31 @@ function PaymentsProcedures({ data }) {
           <Typography
             variant="h4"
             align="center"
-            sx={{ fontWeight: "bold", paddingTop: 1, color: colors.accentGreen[100] }}
+            sx={{
+              fontWeight: "bold",
+              paddingTop: 1,
+              color: colors.accentGreen[100],
+            }}
           >
             GESTIONES Y CUANTAS FUERON PAGADAS
           </Typography>
         </Box>
         {data.length > 0 && (
-          <Bar
-            data={data}
-            index="concept"
+          <ResponsiveBarChart
+            data={formattedData}
+            barColor={({ id, data }) => data[`${id}Color`]}
+            showLegend={false}
+            tooltipFormat={(value) => `${value.toLocaleString()}`}
+            margin={{ top: 30, right: 100, bottom: 50, left: 75 }}
+            backgroundColor="transparent"
             keys={conceptNamesKeys}
-            position="vertical"
-            color="category10"
+            indexBy="concept"
+            axisBottomLegend="dia"
+            axisLeftLegend="Cuentas"
+            axisLeftLegendOffset={-60}
+            showBarValues={false}
+            groupMode="grouped"
+             valueScale ="symlog"
           />
         )}
       </Box>
--- src/components/CoordinationDashboard/PaymentsProceduresByManager.jsx
+++ src/components/CoordinationDashboard/PaymentsProceduresByManager.jsx
@@ -1,19 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { Box, useTheme, Button, Avatar, Typography, LinearProgress, InputAdornment, FormControl, FormHelperText} from "@mui/material";
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
+import React, { useState, useEffect, useMemo } from "react";
+import {
+  Box,
+  useTheme,
+  Button,
+  Avatar,
+  Typography,
+  LinearProgress,
+  InputAdornment,
+  FormControl,
+  FormHelperText,
+} from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
 import { tokens } from "../../theme";
-import { DataGrid,
+import {
+  DataGrid,
   GridToolbarColumnsButton,
-  GridToolbarContainer,  
+  GridToolbarContainer,
   GridToolbarDensitySelector,
-  GridToolbarExport,  
-  GridToolbarFilterButton, } from '@mui/x-data-grid';
-import Viewer from 'react-viewer';
+  GridToolbarExport,
+  GridToolbarFilterButton,
+} from "@mui/x-data-grid";
+import Viewer from "react-viewer";
 import { TaskAlt, Search } from "@mui/icons-material";
 
-function PaymentsProceduresByManager({data}) {
-
+function PaymentsProceduresByManager({ data }) {
   if (!data) {
     return null;
   }
@@ -25,124 +36,156 @@ function PaymentsProceduresByManager({data}) {
   const [matching, setMatching] = useState(-1);
 
   const [noResults, setNoResults] = useState(false);
-  const [searchPerformed, setSearchPerformed] = useState(false);  
+  const [searchPerformed, setSearchPerformed] = useState(false);
 
-  const buildColumns = () => {   
-    const columns = [
-      { 
-        field: 'user',
-        renderHeader: () => (
-          <strong style={{ color: "#5EBFFF" }}>{"NOMBRE"}</strong>
-        ),
+  const buildColumns = useMemo(() => {
+    return [
+      {
+        field: "user",
+        headerName: "NOMBRE",
         width: 270,
         editable: false,
         renderCell: (params) => (
-          <Box sx={{ display: 'flex', alignItems: 'center', p: '12px' }}>
+          <Box sx={{ display: "flex", alignItems: "center", p: "12px" }}>
             <AvatarImage data={params.row.image_user} />
-            <Typography variant="h6" sx={{ marginLeft: 1 }}>{params.value}</Typography>
+            <Typography variant="h6" sx={{ marginLeft: 1 }}>
+              {params.value}
+            </Typography>
           </Box>
-        )
-      }, 
-      { 
-        field: 'count',
-        renderHeader: () => (
-          <strong style={{ color: "#5EBFFF" }}>{"GESTIONES"}</strong>
         ),
-        width: 80,
+      },
+      {
+        field: "count",
+        headerName: "GESTIONES",
+        width: 100,
         editable: false,
-        renderCell: (params) => {          
+        renderCell: (params) => {
           let color = theme.palette.secondary.main;
 
           return (
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>            
-              <Typography variant="body1" sx={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography
+                variant="body1"
+                sx={{ fontWeight: "bold", fontSize: "1.2em" }}
+              >
                 {params.value}
               </Typography>
               <TaskAlt sx={{ color: color }} />
             </div>
-          )          
-        }
-      }, 
-      { 
-        field: 'procedures_with_payment',
-        renderHeader: () => (
-          <strong style={{ color: "#5EBFFF" }}>{"GESTIONES CON PAGO"}</strong>
-        ),
+          );
+        },
+      },
+      {
+        field: "procedures_with_payment",
+        headerName: "CON PAGO",
         width: 150,
         editable: false,
         renderCell: (params) => {
-          
-          const percentage = (params.row.procedures_with_payment / params.row.count) * 100 || 0;
-          
+          const percentage =
+            (params.row.procedures_with_payment / params.row.count) * 100 || 0;
+
           let progressColor;
           if (percentage <= 33) {
-            progressColor = 'error';
+            progressColor = "error";
           } else if (percentage <= 66) {
-            progressColor = 'warning';
+            progressColor = "warning";
           } else {
-            progressColor = 'secondary';
+            progressColor = "secondary";
           }
           return (
-           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '90px' }}>
-             <Typography variant="body1" sx={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%", // Utilizamos todo el ancho disponible
+              }}
+            >
+              <Typography
+                variant="body1"
+                sx={{ fontWeight: "bold", fontSize: "1.2em" }}
+              >
                 {params.value}
               </Typography>
-            <LinearProgress
-              variant="determinate"
-              value={percentage}
-              sx={{ width: '60%', height: '8px' }}
-              style={{ marginTop: '5px' }}
-              color={progressColor}
-            />
-             <Typography variant="body1" sx={{ fontSize: '0.8em' }}>
-             {`${Math.round(percentage)}%`}
-            </Typography>
-          </div>
+              <LinearProgress
+                variant="determinate"
+                value={percentage}
+                sx={{
+                  width: "80%",
+                  height: "10px",
+                  borderRadius: "5px", // Borde redondeado
+                  backgroundColor: "rgba(0, 0, 0, 0.5)", // Fondo suave de la barra
+                }}
+                color={progressColor}
+              />
+              <Typography variant="body1" sx={{ fontSize: "0.8em" }}>
+                {`${Math.round(percentage)}%`}
+              </Typography>
+            </div>
           );
-        }
-      }, 
-      { 
-        field: 'procedures_without_payment',
-        renderHeader: () => (
-          <strong style={{ color: "#5EBFFF" }}>{"GESTIONES SIN PAGO"}</strong>
-        ),
+        },
+      },
+      {
+        field: "procedures_without_payment",
+        headerName: "SIN PAGO",
         width: 150,
         editable: false,
         renderCell: (params) => {
-          
-          const percentage = (params.row.procedures_without_payment / params.row.count) * 100 || 0;
-          
+          const percentage =
+            (params.row.procedures_without_payment / params.row.count) * 100 ||
+            0;
+
           let progressColor;
           if (percentage <= 33) {
-            progressColor = 'error';
+            progressColor = "error";
           } else if (percentage <= 66) {
-            progressColor = 'warning';
+            progressColor = "warning";
           } else {
-            progressColor = 'secondary';
+            progressColor = "secondary";
           }
           return (
-           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '90px' }}>
-             <Typography variant="body1" sx={{ fontWeight: 'bold', fontSize: '1.2em' }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%", // Utilizamos todo el ancho disponible
+              }}
+            >
+              <Typography
+                variant="body1"
+                sx={{ fontWeight: "bold", fontSize: "1.2em" }}
+              >
                 {params.value}
               </Typography>
-            <LinearProgress
-              variant="determinate"
-              value={percentage}
-              sx={{ width: '60%', height: '8px' }}
-              style={{ marginTop: '5px' }}
-              color={progressColor}
-            />
-             <Typography variant="body1" sx={{ fontSize: '0.8em' }}>
-             {`${Math.round(percentage)}%`}
-            </Typography>
-          </div>
+              <LinearProgress
+                variant="determinate"
+                value={percentage}
+                sx={{
+                  width: "80%",
+                  height: "10px",
+                  borderRadius: "5px", // Borde redondeado
+                  backgroundColor: "rgba(0, 0, 0, 0.5)", // Fondo suave de la barra
+                }}
+                color={progressColor}
+              />
+              <Typography variant="body1" sx={{ fontSize: "0.8em" }}>
+                {`${Math.round(percentage)}%`}
+              </Typography>
+            </div>
           );
-        }
-      },     
+        },
+      },
     ];
-  
-    return columns;
-  };
+  }, []);
 
   const AvatarImage = ({ data }) => {
     const [visibleAvatar, setVisibleAvatar] = React.useState(false);
@@ -155,34 +198,35 @@ function PaymentsProceduresByManager({data}) {
           alt="Remy Sharp"
           src={data}
         />
-  
+
         <Viewer
           visible={visibleAvatar}
           onClose={() => {
             setVisibleAvatar(false);
           }}
-          images={[{ src: data, alt: 'avatar' }]}          
+          images={[{ src: data, alt: "avatar" }]}
         />
       </>
     );
   };
   useEffect(() => {
-    
-    setSearchPerformed(true);  
-    
+    setSearchPerformed(true);
+
     if (!searchTerm) {
       setFilteredUsers([]);
       setNoResults(false);
       setMatching(-1);
       return;
-    } 
-    
-    const matchingUsers = data.filter(user => {
-      return Object.values(user).some(value =>
-        value && value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    }
+
+    const matchingUsers = data.filter((user) => {
+      return Object.values(user).some(
+        (value) =>
+          value &&
+          value.toString().toLowerCase().includes(searchTerm.toLowerCase())
       );
-    });  
-    
+    });
+
     if (matchingUsers.length === 0) {
       setFilteredUsers([]);
       setNoResults(true);
@@ -192,9 +236,7 @@ function PaymentsProceduresByManager({data}) {
       setNoResults(false);
       setMatching(matchingUsers.length);
     }
-  
   }, [searchTerm]);
-  
 
   useEffect(() => {
     setNoResults(false);
@@ -211,67 +253,140 @@ function PaymentsProceduresByManager({data}) {
       gridTemplateColumns="repeat(12, 1fr)"
       gridAutoRows="480px"
       gap="15px"
-    >   
+    >
       <Box
-        gridColumn='span 12'
-        backgroundColor='rgba(128, 128, 128, 0.1)'
+        gridColumn="span 12"
+        backgroundColor="rgba(128, 128, 128, 0.1)"
         borderRadius="10px"
-        sx={{ cursor: 'pointer' }}
+        sx={{ cursor: "pointer" }}
       >
         {data.length > 0 && (
           <>
-          <Grid item xs={12} container justifyContent="space-between" alignItems="stretch" spacing={2} >
-            <Grid item xs={12}>
-              <Typography 
-                variant="h4" 
-                align="center" 
-                sx={{ fontWeight: 'bold', paddingTop: 1 }}
-              >
-                GESTIONES PAGADAS
-              </Typography>
-            </Grid>
-            <Grid item xs={6} sx={{ paddingBottom: 1 }}>
-              <FormControl>
-                <TextField                              
-                  fullWidth                            
-                  value={searchTerm}              
-                  onChange={handleChange}              
-                  color='secondary'
-                  size="small"
-                  placeholder="Ingresa tu búsqueda"
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <Search color="secondary"/>
-                      </InputAdornment>
-                    ),
+            <Grid
+              item
+              xs={12}
+              container
+              justifyContent="space-between"
+              alignItems="stretch"
+              spacing={2}
+            >
+              <Grid item xs={12}>
+                <Typography
+                  variant="h4"
+                  align="center"
+                  sx={{
+                    fontWeight: "bold",
+                    paddingTop: 1,
+                    color: colors.accentGreen[100],
                   }}
-                />
-                
-                {( noResults ) && (
-                  <FormHelperText  style={{ color: 'red' }}>
-                    No se encontraron resultados
-                  </FormHelperText>
-                )}
-                
-              </FormControl>
+                >
+                  GESTIONES PAGADAS
+                </Typography>
+              </Grid>
+              <Grid item xs={6} sx={{ paddingBottom: 1 }}>
+                <FormControl>
+                  <TextField
+                    label="Busqueda"
+                    fullWidth
+                    value={searchTerm}
+                    onChange={handleChange}
+                    color="secondary"
+                    size="small"
+                    placeholder="Ingresa tu búsqueda"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <Search sx={{ color: colors.accentGreen[100] }} />
+                        </InputAdornment>
+                      ),
+                    }}
+                    sx={{
+                      "& .MuiOutlinedInput-root": {
+                        borderRadius: "20px", // Bordes redondeados
+                        "& .MuiOutlinedInput-notchedOutline": {
+                          borderColor: colors.accentGreen[100], // Color predeterminado del borde
+                        },
+                        "&:hover .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "accent.light", // Color al pasar el mouse
+                        },
+                        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                          borderColor: "accent.dark", // Color al enfocar
+                        },
+                      },
+                    }}
+                  />
+
+                  {noResults && (
+                    <FormHelperText style={{ color: "red" }}>
+                      No se encontraron resultados
+                    </FormHelperText>
+                  )}
+                </FormControl>
+              </Grid>
             </Grid>
-           </Grid>
-            <Grid item xs={12} container justifyContent="space-between" alignItems="stretch" spacing={2}>              
-              <Grid item xs={12} style={{ height: 400, width: '100%' }}>         
+            <Grid
+              item
+              xs={12}
+              container
+              justifyContent="space-between"
+              alignItems="stretch"
+              spacing={2}
+            >
+              <Grid item xs={12} style={{ height: 400, width: "100%" }}>
                 <DataGrid
-                    rows={filteredUsers.length > 0 ? filteredUsers : data}
-                    columns={buildColumns()}
-                    getRowId={(row) => row.id}
-                    editable={false}                 
-                    autoPageSize
+                  rows={filteredUsers.length > 0 ? filteredUsers : data}
+                  columns={buildColumns.map((column) => ({
+                    ...column,
+                    renderHeader: () => (
+                      <Typography
+                        sx={{
+                          color: colors.contentSearchButton[100],
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {column.headerName}
+                      </Typography>
+                    ),
+                  }))}
+                  getRowId={(row) => row.id}
+                  editable={false}
+                  autoPageSize
+                  sx={{
+                    borderRadius: "8px",
+                    boxShadow: 3,
+                    padding: 0,
+                    background: "rgba(128, 128, 128, 0.1)",
+                    "& .MuiDataGrid-columnHeaders": {
+                      backgroundColor: colors.accentGreen[100], // Color de fondo deseado
+                      borderTopLeftRadius: "8px",
+                      borderTopRightRadius: "8px",
+                    },
+                    "& .MuiDataGrid-footerContainer": {
+                      borderBottomLeftRadius: "8px",
+                      borderBottomRightRadius: "8px",
+                      backgroundColor: colors.accentGreen[100], // Fondo del footer (paginador)
+                      color: colors.contentSearchButton[100], // Color de texto dentro del footer
+                    },
+                    "& .MuiTablePagination-root": {
+                      color: colors.contentSearchButton[100], // Color del texto del paginador
+                    },
+                    // Estilos específicos para los íconos en el encabezado y pie de página
+                    "& .MuiDataGrid-columnHeaders .MuiSvgIcon-root, .MuiDataGrid-footerContainer .MuiSvgIcon-root":
+                      {
+                        color: colors.contentSearchButton[100], // Color de los íconos (flechas)
+                      },
+                    // Evitar que los íconos en las celdas se vean afectados
+                    "& .MuiDataGrid-cell .MuiSvgIcon-root": {
+                      color: "inherit", // No afectar el color de los íconos en las celdas
+                    },
+                  }}
                 />
               </Grid>
             </Grid>
           </>
-        )}  
+        )}
       </Box>
-  </Box>   
-  )
+    </Box>
+  );
 }
-export default PaymentsProceduresByManager
+export default PaymentsProceduresByManager;
